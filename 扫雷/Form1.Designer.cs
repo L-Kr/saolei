21dbb86da9feb2a0f8027d39fff1d9e2d2ab6d5b
Li Rui <987748679@qq.com>
2018-05-24 15:37:53 +0800

    Initial commit

---
+++ 扫雷/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace 扫雷
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.难度选择ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eazy10104ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normal404016ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lunatic10010050ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.难度选择ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(401, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 难度选择ToolStripMenuItem
+            // 
+            this.难度选择ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eazy10104ToolStripMenuItem,
+            this.normal404016ToolStripMenuItem,
+            this.hardToolStripMenuItem,
+            this.lunatic10010050ToolStripMenuItem});
+            this.难度选择ToolStripMenuItem.Name = "难度选择ToolStripMenuItem";
+            this.难度选择ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.难度选择ToolStripMenuItem.Text = "难度选择";
+            this.难度选择ToolStripMenuItem.Click += new System.EventHandler(this.难度选择ToolStripMenuItem_Click);
+            // 
+            // eazy10104ToolStripMenuItem
+            // 
+            this.eazy10104ToolStripMenuItem.Name = "eazy10104ToolStripMenuItem";
+            this.eazy10104ToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.eazy10104ToolStripMenuItem.Text = "eazy(10*10,4)";
+            this.eazy10104ToolStripMenuItem.Click += new System.EventHandler(this.eazy10104ToolStripMenuItem_Click);
+            // 
+            // normal404016ToolStripMenuItem
+            // 
+            this.normal404016ToolStripMenuItem.Name = "normal404016ToolStripMenuItem";
+            this.normal404016ToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.normal404016ToolStripMenuItem.Text = "normal(40*40,16)";
+            this.normal404016ToolStripMenuItem.Click += new System.EventHandler(this.normal404016ToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.hardToolStripMenuItem.Text = "hard(60*60,24)";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
+            // lunatic10010050ToolStripMenuItem
+            // 
+            this.lunatic10010050ToolStripMenuItem.Name = "lunatic10010050ToolStripMenuItem";
+            this.lunatic10010050ToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.lunatic10010050ToolStripMenuItem.Text = "lunatic(100*100,50)";
+            this.lunatic10010050ToolStripMenuItem.Click += new System.EventHandler(this.lunatic10010050ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 403);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 难度选择ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normal404016ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lunatic10010050ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eazy10104ToolStripMenuItem;
+    }
+}
+
---
+++ 扫雷/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 扫雷
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public  int Num = 0;
+        public  int Bumb = 0;
+        public int[,] arr;
+        public Button[,] but;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void 难度选择ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eazy10104ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Num = 10;
+            Bumb = 10;
+            arr = new int[Num, Num];
+            but = new Button[Num, Num];
+            start();
+        }
+
+        private void normal404016ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Num = 16;
+            Bumb = 40;
+            arr = new int[Num, Num];
+            but = new Button[Num, Num];
+            start();
+        }
+
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Num = 24;
+            Bumb = 60;
+            arr = new int[Num, Num];
+            but = new Button[Num, Num];
+            start();
+        }
+
+        private void lunatic10010050ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Num = 50;
+            Bumb = 100;
+            arr = new int[Num, Num];
+            but = new Button[Num, Num];
+            start();
+        }
+
+        public void start()
+        {
+
+            {
+
+                /*埋雷*/
+                for (int i = 0; i < Num; i++)
+                    for (int j = 0; j < Num; j++)
+                        arr[i, j] = 0;
+                for (int k = 0; k < Bumb; k++)
+                {
+                    Random ri = new Random(Guid.NewGuid().GetHashCode());
+                    Random rj = new Random(Guid.NewGuid().GetHashCode());
+                    int i = ri.Next(Num);
+                    int j = rj.Next(Num);
+                    if (arr[i, j] == -1)
+                        k--;
+                    else
+                    {
+                        arr[i, j] = -1;
+                        if (i != 0)
+                        {
+                            if (arr[i - 1, j] != -1)
+                                arr[i - 1, j]++;
+                        }
+                        if (i != Num - 1)
+                        {
+                            if (arr[i + 1, j] != -1)
+                                arr[i + 1, j]++;
+                        }
+                        if (j != 0)
+                        {
+                            if (arr[i, j - 1] != -1)
+                                arr[i, j - 1]++;
+                        }
+                        if (j != Num - 1)
+                        {
+                            if (arr[i, j + 1] != -1)
+                                arr[i, j + 1]++;
+                        }
+                        if (i != 0 && j != 0)
+                        {
+                            if (arr[i - 1, j - 1] != -1)
+                                arr[i - 1, j - 1]++;
+                        }
+                        if (i != Num - 1 && j != Num - 1)
+                        {
+                            if (arr[i + 1, j + 1] != -1)
+                                arr[i + 1, j + 1]++;
+                        }
+                        if (i != Num - 1 && j != 0)
+                        {
+                            if (arr[i + 1, j - 1] != -1)
+                                arr[i + 1, j - 1]++;
+                        }
+                        if (i != 0 && j != Num - 1)
+                        {
+                            if (arr[i - 1, j + 1] != -1)
+                                arr[i - 1, j + 1]++;
+                        }
+                    }
+                }
+
+                /*按钮设计*/
+                for (int i = 0; i < Num; i++)
+                    for (int j = 0; j < Num; j++)
+                        but[i, j] = new Button();
+                foreach (Button b in but)
+                {
+                    b.BackColor = Color.SpringGreen;
+                    b.ForeColor = SystemColors.ControlText;
+                    b.Size = new Size(25, 25);
+                    b.TabIndex = 0;
+                    b.UseVisualStyleBackColor = false;
+                    b.Enabled = true;
+                    Controls.Add(b);
+                }
+                but[0, 0].Location = new Point(0, 25);
+                but[0, 0].Name = "but[0,0]";
+                for (int i = 0; i < Num; i++)
+                {
+                    for (int j = 1; j < Num; j++)
+                    {
+                        but[i, j].Location = new Point(but[i, j - 1].Location.X + 25, but[i, j - 1].Location.Y);
+                        but[i, j].Name = "but[" + i.ToString() + "," + j.ToString() + "]";
+                    }
+                    if (i < Num - 1)
+                    {
+                        but[i + 1, 0].Location = new Point(but[i, 0].Location.X, but[i, 0].Location.Y + 25);
+                        but[i + 1, 0].Name = "but[" + i + 1.ToString() + ",0]";
+                    }
+                }
+                /*for (int i = 0; i < Num; i++)
+                {
+                    for (int j = 0; j < Num; j++)
+                    {
+                        but[i, j].Click += (sender1, e1) =>
+                        {
+                            MessageBox.Show(arr[i, j].ToString());
+                        };
+                    }
+                }*/
+                foreach (Button b in but)
+                {
+                    b.MouseDown += (sender2, e2) =>
+                    {
+                        if (e2.Button == MouseButtons.Left)
+                        {
+                            int i = (b.Location.Y / 25) - 1;    /*按钮边长为25,菜单栏宽25*/
+                            int j = b.Location.X / 25;    /*按钮边长为25*/
+                            if (arr[i, j] != 0 && arr[i, j] != -1)
+                                b.Text = arr[i, j].ToString();
+                            b.BackColor = Color.AntiqueWhite;
+                            b.Enabled = false;
+                            if (arr[i, j] == -1)
+                            {
+                                MessageBox.Show("游戏结束");
+                                Dispose();
+                            }
+
+                            /*当被点区域周围没有雷的时候*/
+                            if (arr[i, j] == 0)
+                            {
+                                Stack<int> si = new Stack<int>();
+                                Stack<int> sj = new Stack<int>();
+                                si.Push(i);
+                                sj.Push(j);
+                                do
+                                {
+                                    i = si.Pop();
+                                    j = sj.Pop();
+                                    if (i != 0)
+                                    {
+                                        if (but[i - 1, j].Enabled == true)
+                                        {
+                                            but[i - 1, j].BackColor = Color.AntiqueWhite;
+                                            but[i - 1, j].Enabled = false;
+                                            if (arr[i - 1, j] == 0)
+                                            {
+                                                si.Push(i - 1);
+                                                sj.Push(j);
+                                            }
+                                            else
+                                                but[i - 1, j].Text = arr[i - 1, j].ToString();
+                                        }
+                                    }
+                                    if (i != Num - 1)
+                                    {
+                                        if (but[i + 1, j].Enabled == true)
+                                        {
+                                            but[i + 1, j].BackColor = Color.AntiqueWhite;
+                                            but[i + 1, j].Enabled = false;
+                                            if (arr[i + 1, j] == 0)
+                                            {
+                                                si.Push(i + 1);
+                                                sj.Push(j);
+                                            }
+                                            else
+                                                but[i + 1, j].Text = arr[i + 1, j].ToString();
+                                        }
+                                    }
+                                    if (j != 0)
+                                    {
+                                        if (but[i, j - 1].Enabled == true)
+                                        {
+                                            but[i, j - 1].BackColor = Color.AntiqueWhite;
+                                            but[i, j - 1].Enabled = false;
+                                            if (arr[i, j - 1] == 0)
+                                            {
+                                                si.Push(i);
+                                                sj.Push(j - 1);
+                                            }
+                                            else
+                                                but[i, j - 1].Text = arr[i, j - 1].ToString();
+                                        }
+                                    }
+                                    if (j != Num - 1)
+                                    {
+                                        if (but[i, j + 1].Enabled == true)
+                                        {
+                                            but[i, j + 1].BackColor = Color.AntiqueWhite;
+                                            but[i, j + 1].Enabled = false;
+                                            if (arr[i, j + 1] == 0)
+                                            {
+                                                si.Push(i);
+                                                sj.Push(j + 1);
+                                            }
+                                            else
+                                                but[i, j + 1].Text = arr[i, j + 1].ToString();
+                                        }
+                                    }
+                                    if (i != 0 && j != 0)
+                                    {
+                                        if (but[i - 1, j - 1].Enabled == true)
+                                        {
+                                            but[i - 1, j - 1].BackColor = Color.AntiqueWhite;
+                                            but[i - 1, j - 1].Enabled = false;
+                                            if (arr[i - 1, j - 1] == 0)
+                                            {
+                                                si.Push(i - 1);
+                                                sj.Push(j - 1);
+                                            }
+                                            else
+                                                but[i - 1, j - 1].Text = arr[i - 1, j - 1].ToString();
+                                        }
+                                    }
+                                    if (i != Num - 1 && j != Num - 1)
+                                    {
+                                        if (but[i + 1, j + 1].Enabled == true)
+                                        {
+                                            but[i + 1, j + 1].BackColor = Color.AntiqueWhite;
+                                            but[i + 1, j + 1].Enabled = false;
+                                            if (arr[i + 1, j + 1] == 0)
+                                            {
+                                                si.Push(i + 1);
+                                                sj.Push(j + 1);
+                                            }
+                                            else
+                                                but[i + 1, j + 1].Text = arr[i + 1, j + 1].ToString();
+                                        }
+                                    }
+                                    if (i != Num - 1 && j != 0)
+                                    {
+                                        if (but[i + 1, j - 1].Enabled == true)
+                                        {
+                                            but[i + 1, j - 1].BackColor = Color.AntiqueWhite;
+                                            but[i + 1, j - 1].Enabled = false;
+                                            if (arr[i + 1, j - 1] == 0)
+                                            {
+                                                si.Push(i + 1);
+                                                sj.Push(j - 1);
+                                            }
+                                            else
+                                                but[i + 1, j - 1].Text = arr[i + 1, j - 1].ToString();
+                                        }
+                                    }
+                                    if (i != 0 && j != Num - 1)
+                                    {
+                                        if (but[i - 1, j + 1].Enabled == true)
+                                        {
+                                            but[i - 1, j + 1].BackColor = Color.AntiqueWhite;
+                                            but[i - 1, j + 1].Enabled = false;
+                                            if (arr[i - 1, j + 1] == 0)
+                                            {
+                                                si.Push(i - 1);
+                                                sj.Push(j + 1);
+                                            }
+                                            else
+                                                but[i - 1, j + 1].Text = arr[i - 1, j + 1].ToString();
+                                        }
+                                    }
+                                } while (si.Count != 0 && sj.Count != 0);
+                            }
+
+                            int x = 0;
+                            foreach (Button ba in but)
+                            {
+                                if (ba.Enabled == true)
+                                    x++;
+                            }
+                            if (x == Bumb)
+                            {
+                                MessageBox.Show("恭喜通关！");
+                                Dispose();
+                            }
+                        }
+                        else if (e2.Button == MouseButtons.Right)
+                        {
+                            b.Text = "*";
+                        }
+                    };
+
+                }
+            }
+        }
+    }
+}
